7bd0c22b8d3a8e751e6b2eaef3e8ccf154618f41
Jigar Solanki <jigar234@gmail.com>
2012-04-22 18:39:02 -0400

    Show object count

--- static/jasmine/closure_reporter.js
+++ static/jasmine/closure_reporter.js
@@ -3,15 +3,20 @@ goog.require('goog.events');
 goog.require('goog.ui.Control');
 
 jasmine.ClosureReporter = function() {
+  goog.Disposable.ENABLE_MONITORING = true;
 };
 
 jasmine.ClosureReporter.prototype.reportRunnerResults = function(spec) {
 
-  var beforeDiv, content;
+  var eventDiv, content, objectDiv;
 
-  beforeDiv = document.createElement('div');
-  beforeDiv['className'] = "eventCounter";
+  eventDiv = goog.dom.createDom('div', "eventCounter");
   content = "Total Event Listeners: " + goog.events.getTotalListenerCount();
-  goog.dom.setTextContent(beforeDiv, content);
-  goog.dom.appendChild(document.body,beforeDiv);
+  goog.dom.setTextContent(eventDiv, content);
+  goog.dom.appendChild(document.body,eventDiv);
+
+  objectDiv = goog.dom.createDom('div', "objectCounter");
+  content = "Total Undisposed Objects: " + goog.Disposable.getUndisposedObjects().length;
+  goog.dom.setTextContent(objectDiv, content);
+  goog.dom.appendChild(document.body,objectDiv);
 };
