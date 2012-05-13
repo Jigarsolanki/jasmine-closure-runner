c603898821002b280bac456af0677c2801386215
Jigar Solanki <jigar234@gmail.com>
2012-05-13 15:07:11 -0400

    Fix count

--- app.js
+++ app.js
@@ -15,26 +15,32 @@ app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function (req, res, next) {
 
-  var namespace, specWalker, walker;
+  var specPath, specWalker, walker;
 
-  namespace = req.param('path');
-  if(namespace) {
-    specWalker = new SpecWalker(specDirPath);
-    walker = specWalker.getSpecFilesByRootDir(namespace);
+  specPath = req.param('path');
+  specWalker = new SpecWalker(specDirPath);
+  if(specPath) {
+    walker = specWalker.getSpecFilesByRootDir(specPath);
     walker.on('finished', function(data){
-      walker2 = specWalker.generateJson();
-      walker2.on('finished', function(treeData){
-        res.render('spec', {
-          'namespace': namespace,
-          'files': data,
-          'specTree': treeData
-        });
-      });
+      renderResponse(res, specWalker, data, specPath);
     });
   } else {
-    res.end();
+    renderResponse(res, specWalker, [], specPath);
   }
 });
 
+renderResponse = function(res, specWalker, data, specPath) {
+
+  var walker;
+
+  walker = specWalker.generateJson();
+  walker.on('finished', function(treeData){
+    res.render('spec', {
+      'specPath': specPath,
+      'files': data,
+      'specTree': treeData
+    });
+  });
+}
 app.listen(nconf.get('port'));
 console.log('\nserver started at http://localhost:' + nconf.get('port'));
--- static/jasmine/closure_reporter.js
+++ static/jasmine/closure_reporter.js
@@ -6,12 +6,17 @@ goog.require('goog.ui.Control');
 jasmine.ClosureReporter = function() {
   //goog.Disposable.ENABLE_MONITORING = true;
 };
+jasmine.ClosureReporter.prototype.eventCountBefore_ = 0;
+
+jasmine.ClosureReporter.prototype.reportRunnerStarting = function(runner){
+  this.eventCountBefore_ =  goog.events.getTotalListenerCount();
+};
 
 jasmine.ClosureReporter.prototype.reportRunnerResults = function(spec) {
 
   var eventDiv, content, objectDiv, listenerCount;
 
-  listenerCount = goog.events.getTotalListenerCount();
+  listenerCount = goog.events.getTotalListenerCount() - this.eventCountBefore_;
   eventDiv = goog.dom.getElement('total_events');
   goog.dom.setTextContent(eventDiv, listenerCount);
   if(listenerCount > 0) {
